7733bb3bed4ecbd265e5d598fa488a2479804a66
emiranivala <taitufam@gmail.com>
2025-03-05 21:42:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ BOT_TOKEN = getenv("BOT_TOKEN", "")
 OWNER_ID = list(map(int, getenv("OWNER_ID", "922270982 7446465090").split()))
 MONGO_DB = getenv("MONGO_DB", "")
 
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002294535138"))
-PREMIUM_LOGS = int(getenv("PREMIUM_LOGS", "-1002458919549"))
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002458919549"))
+PREMIUM_LOGS = int(getenv("PREMIUM_LOGS", "-1002294535138"))
 
 
